0a6ba4dd8f457073165c5559d6bba3c9cfc244c5
EDWIN WILLER NARVAEZ <ewnarvaez@gmail.com>
2021-07-01 13:54:48 -0500

    header finalizado

--- app/public/wp-content/themes/lapizzeria/functions.php
+++ app/public/wp-content/themes/lapizzeria/functions.php
@@ -3,6 +3,7 @@
 /** CSS y JavaScript **/
 function lapizzeria_styles() {
 
+	/** CSS **/
 	// normalize
 	wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
 
@@ -11,5 +12,26 @@ function lapizzeria_styles() {
 
 	// GoogleFonts
 	wp_enqueue_style( 'googleFonts', 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@400;700;900&family=Staatliches&display=swap', array(), '1.0.0');
+
+	// slicknav_css
+	wp_enqueue_style( 'slicknavCSS', get_template_directory_uri() . '/css/slicknav.min.css', array(), '1.0.10');
+
+
+	/** JS **/
+	// slicknav javascript
+	wp_enqueue_script('slicknavJS', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array('jquery'), '1.0.10', true);
+
+	// Script propio
+	wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'lapizzeria_styles' );
+
+/** MENÚS **/
+function lapizzeria_menus() {
+	register_nav_menus( array( 
+		'header-menu' => 'Header Menu',
+		'redes-sociales' => 'Redes Sociales'
+	) );
+}
+add_action( 'init', 'lapizzeria_menus');
+
---
+++ app/public/wp-content/themes/lapizzeria/js/scripts.js
@@ -0,0 +1,7 @@
+jQuery(document).ready( $ => {
+	// Menu responsive
+	$('#menu ul').slicknav({
+		// para poner el menu debajo del site-header
+		appendTo: $('.site-header')
+	});
+});
