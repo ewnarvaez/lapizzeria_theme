a2d8c92d44f5f23f951edb16eacfb866fd70390a
EDWIN WILLER NARVAEZ <ewnarvaez@gmail.com>
2021-07-01 18:42:09 -0500

    Footer finalizado

--- app/public/wp-content/themes/lapizzeria/footer.php
+++ app/public/wp-content/themes/lapizzeria/footer.php
@@ -1,5 +1,20 @@
 
+		<footer class="site-footer">
+			<?php  
+			$args = array(
+					'theme_location' => 'header-menu',
+					'container' => 'nav',
+					'container_class' => 'footer-nav',
+					'after' => '<span class="separador"> | </span>'
+				);
+				wp_nav_menu($args);
+			?>
 
+			<div class="direccion">
+				<p>8179 Bay Avenue Mountain View, CA 94043</p>
+				<p>Teléfono: 9856248235</p>
+			</div>
+		</footer>
 		<?php wp_footer(); ?>
 	</body>
 </html>
